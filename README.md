e4ec89c99f5772b42d685f928a80be5a2ec17eb0
Anmol Agarwal <aagarwal622@gatech.edu>
2023-10-16 00:25:48 -0400

    Almost complete implementation

--- README.md
+++ README.md
@@ -10,6 +10,7 @@
 Above was the most basic implementation as needed. Now we talk about additional features which we can add:
 1. Use LLM to summarize the relevant paragraphs for the user, so that they can also get summary for their query.
 2. Use ChatGPT or GPT4All to also summarize the documents in 1 line for the user and store in MyPDFs. If user asks for summary, just output the summary of all documents.
+3. Also support for txt documents. Straightforward!! Just add their data into MyDocuments. [One caveat: it seems we can't insert into MyPDFs, so probably do insert into type command from MyPDFs into global MyDocuments table]
 
 ## Additional Details
 Will think if we can add more features like document clustering and more advanced queries like “Tell me about spring in context of computer science”, this will only get results from documents clustered under computer science and give info about spring framework for Java. Or “Tell me about spring in context of nature”, this will get results documents clustered under nature and give info about the spring season. Otherwise this can be taken up in Project 2.
@@ -17,5 +18,5 @@ Will think if we can add more features like document clustering and more advance
 Use different Sentence Feature Extractor through different model (probably OpenAI) and qualitatively compare the output.
 
 ## Status
-Most of the above implemented, except for using ChatGPT/GPT4All to summarize each row in 1 line in MyPDFs. Perhaps can create an abstract function for that.
-Also need to check the correctness of the algorithm and run it.
+Most of the above implemented, except for using ChatGPT/GPT4All to summarize each row in 1 line in MyDocuments. Perhaps can create an abstract function for that.
+Also need to check the correctness of the algorithm and run it. Also merge pdfs/text together.
--- ai_search_engine.py
+++ ai_search_engine.py
@@ -2,8 +2,18 @@ import evadb
 import os
 import pandas as pd
 from collections import OrderedDict
+import warnings
+
+"""
+        AI Search Engine
+        Author: Anmol Agarwal (aagarwal622)
+        Date: 10.15.2023
+"""
+
+warnings.filterwarnings("ignore")
 
 # This cursor will be used through the whole code to run AI queries
+print("Connecting to evaDB")
 cursor = evadb.connect().cursor()
 
 # Few constants
@@ -12,6 +22,39 @@ PARAGRAPHS_LIMIT = 10
 DEFAULT_PDFS_PATH = "./pdfs"
 DEFAULT_SENTENCE_FEATURE_EXTRACTOR_PATH = "./functions/sentence_feature_extractor.py"
 
+# Break txt document into paragraphs (similar to how we do for pdf) and insert into my Documents.
+def insert_text_file(name: str, path: str) -> None:
+    try:
+        file = open(path, "r")
+    except:
+        print("Unable to open file {}".format(path))
+
+    data = file.read().split("\n")
+
+    values = []
+    for i in range(len(data)):
+        temp_data = "'{}', 1, {}, '{}'".format(name, i+1, data[i])
+        if i == 0:
+            values.append("("+temp_data)
+        elif i==len(data)-1:
+            values.append(temp_data+");")
+        else:
+            values.append(temp_data)
+
+    values = "),\n(".join(values)
+
+    # insertion query
+    insert_query = f"""
+        INSERT INTO MyDocument(name, page, paragraph, data) VALUES
+        {values}
+    """
+    cursor.query(insert_query).df()
+
+# this function will take care of merging PDFs into overall MyDocuments table containing both text and pdf data
+def merge_pdfs():
+    # TODO: Merge MyPDFs into MyDocuments table
+    pass
+
 # Create database MyDocuments from files stored in pdfs/
 def create_my_documents(path: str = DEFAULT_PDFS_PATH) -> None:
     drop_table_query = "DROP TABLE IF EXISTS MyDocuments"
@@ -19,19 +62,27 @@ def create_my_documents(path: str = DEFAULT_PDFS_PATH) -> None:
     # drop table if exists
     cursor.query(drop_table_query).df()
     files = os.listdir(path)
-    for pdf in files:
-        if pdf.endswith(".pdf"):
+    for file in files:
+        if file.endswith(".pdf"):
             # add each pdf file while also checking extension
-            cursor.query(load_pdfs_query.format(path, pdf)).df()
+            cursor.query(load_pdfs_query.format(path, file)).df()
+        elif file.endswith(".txt"):
+            # add each text file by calling insert_text_file
+            # insert_text_file(f"{path}/file")
+            pass
+
+    print("MyDocuments table initialized")
 
 # Create sentence feature extractor function
 def create_sentence_feature_extractor(path: str = DEFAULT_SENTENCE_FEATURE_EXTRACTOR_PATH) -> None:
     drop_function_query = "DROP FUNCTION IF EXISTS SentenceFeatureExtractor"
-    create_function_query = "CREATE FUNCTION SentenceFeatureExtractor IMPL {}".format(path)
+    create_function_query = "CREATE FUNCTION SentenceFeatureExtractor IMPL '{}'".format(path)
     # drop SentenceFeatureExtractor function if exists
     cursor.query(drop_function_query).df()
     # now create SentenceFeatureExtractor Function
     cursor.query(create_function_query).df()
+
+    print("Sentence Feature Extractor created")
 
 # Create vector index for embedding entries in MyDocuments database, so that we can run fast queries later on.
 def create_embeddings_vector_index() -> None:
@@ -41,6 +92,8 @@ def create_embeddings_vector_index() -> None:
         USING QDRANT
     """
     cursor.query(create_vector_index_query).df()
+
+    print("Vector Index for SentenceFeatureExtractor created")
 
 
 # Now we get results from the MyDocuments table on incoming query.
@@ -56,7 +109,7 @@ def get_query_results(search_query: str, limit: int = PARAGRAPHS_LIMIT) -> Order
     result_query = f"""
         SELECT scored_paragraphs.name, scored_paragraphs.paragraph, scored_paragraphs.data, scored_paragraphs.distance
         FROM (
-            SELECT name, paragraph, data, Similarity(SentenceFeatureExtractor({search_query}), SentenceFeatureExtractor(data)) 
+            SELECT name, paragraph, data, Similarity(SentenceFeatureExtractor('{search_query}'), SentenceFeatureExtractor(data)) 
             FROM MyDocuments
         ) AS scored_paragraphs
         WHERE scored_paragraphs.distance >= {SIMILARITY_THRESHOLD}
@@ -70,7 +123,7 @@ def get_query_results(search_query: str, limit: int = PARAGRAPHS_LIMIT) -> Order
     documents_dictionary = OrderedDict()
 
     for index, row in scored_documents_df.iterrows():
-        documents_dictionary[row["scored_paragraphs.name"]]=[]
+        documents_dictionary[index]=[]
 
     for index, row in scored_paragraphs_df.iterrows():
         document_name = row["scored_paragraphs.name"]
@@ -106,26 +159,32 @@ def summarize_with_LLM(documents_dictionary: OrderedDict) -> str:
     # Since we want to apply text summarizer, we will need to create a table containing the above prompt
     # and run the text summarizer on that
 
-    # table creation query
+    # table creation query (TEXT(0) didn't really limit the length of data stored)
     create_table_query = """
         CREATE TABLE PromptData
         (id INTEGER,
-        data TEXT);
+        data TEXT(0));
     """
 
     # prompt insertion query
     insert_prompt_query = f"""
         INSERT INTO PromptData (id, data) VALUES
-        (1, {prompt});
+        (1, '{prompt}');
     """
 
     # after this, we will call table function to summarize the prompt using bart LLM (can try with others too)
     summarization_query = "SELECT TextSummarizer(data) FROM PromptData"
 
+    # drop table query
+    drop_table_query = """
+        DROP TABLE IF EXISTS PromptData
+    """
+
     # run the queries
     cursor.query(create_table_query).df()
     cursor.query(insert_prompt_query).df()
     df = cursor.query(summarization_query).df()
+    cursor.query(drop_table_query).df()
 
     return df.iloc[0, 0]
 
@@ -138,6 +197,7 @@ def create_text_summarizer():
         MODEL 'facebook/bart-large-cnn'
     """
     cursor.query(create_summarizer_query).df()
+    print("Text summarizer created")
 
 # initialize all tables and functions
 def initialize():
@@ -151,6 +211,7 @@ def process_query():
     limit = PARAGRAPHS_LIMIT
     summarize = False
     initialize()
+    print("Initialized all tables and functions")
     while True:
         query = input("Query: ")
         if query == "exit":
@@ -178,3 +239,9 @@ def process_query():
             print(summarize_with_LLM(documents_dictionary))
         print(return_results(documents_dictionary))
         print("______________________________________________________________________")
+
+
+# Main program running process_query to get queries from users.
+if __name__ == "__main__":
+    print("Engine started")
+    process_query()
