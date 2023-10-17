c4687271aba07d7fdbe456b0eed453226897cf0c
Anmol Agarwal <aagarwal622@gatech.edu>
2023-10-17 18:38:10 -0400

    Completed

--- ai_search_engine.py
+++ ai_search_engine.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 from collections import OrderedDict
 import warnings
-import threading
+import time
 
 """
         AI Search Engine
@@ -17,23 +17,24 @@ print("Connecting to evaDB")
 cursor = evadb.connect().cursor()
 
 # Few global variables and constants
-SIMILARITY_THRESHOLD = 0.7
+SIMILARITY_THRESHOLD = 1.0
 PARAGRAPHS_LIMIT = 10
-DEFAULT_DOCS_PATH = "./docs"
+DEFAULT_DOCS_PATH = "docs"
 DEFAULT_SENTENCE_FEATURE_EXTRACTOR_PATH = "./functions/sentence_feature_extractor.py"
 user_defined_limit = PARAGRAPHS_LIMIT
 user_defined_summarize = False
-polling_lock = threading.Lock()
-polling_interval = 10
+polling_interval = 60
+
 
 # Break txt document into paragraphs (similar to how we do for pdf) and insert into my Documents.
-def insert_text_file(name: str, path: str) -> None:
+def insert_text_file(path: str) -> None:
+    data = ""
     try:
         file = open(path, "r")
+        data = file.read().split("\n")
     except:
         print("Unable to open file {}".format(path))
-
-    data = file.read().split("\n")
+        return
 
     # insertion query
     insert_query = """
@@ -46,7 +47,7 @@ def insert_text_file(name: str, path: str) -> None:
         data[i] = data[i].strip()
         if data[i].isspace() or len(data[i]) == 0:
             continue
-        values = "('{}', 1, {}, '{}')".format(name, count, data[i])
+        values = "('{}', 1, {}, '{}')".format(path, count, data[i])
         # print("New insert query: {}".format(insert_query.format(values)))
         cursor.query(insert_query.format(values)).df()
         count += 1
@@ -64,41 +65,56 @@ def merge_pdfs():
 
     # insert all data from MyPDFs into MyDocuments
     for index, row in mypdfs_df.iterrows():
-        cursor.query(insert_query.format(row['mypdfs.name'], row['mypdfs.page'], row['mypdfs.paragraph'], row['mypdfs.data'])).df()
+        cursor.query(insert_query.format(row['mypdfs.name'], row['mypdfs.page'], row['mypdfs.paragraph'],
+                                         row['mypdfs.data'])).df()
 
 
-# Create database MyDocuments from files stored in pdfs/
-def create_my_documents(path: str = DEFAULT_DOCS_PATH) -> None:
+def read_and_store_documents(path: str = DEFAULT_DOCS_PATH) -> None:
     drop_table_query = "DROP TABLE IF EXISTS MyDocuments"
     drop_pdfs_query = "DROP TABLE IF EXISTS MyPDFs"
     create_table_query = """
-        CREATE TABLE MyDocuments (
-            name TEXT(0),
-            page INTEGER,
-            paragraph INTEGER,
-            data TEXT(0)
-        );
-    """
-    load_pdfs_query = "LOAD PDF '{}/{}' INTO MyPDFs"
+            CREATE TABLE MyDocuments (
+                name TEXT(0),
+                page INTEGER,
+                paragraph INTEGER,
+                data TEXT(0)
+            );
+        """
+
     # drop table if exists
     cursor.query(drop_table_query).df()
     cursor.query(drop_pdfs_query).df()
 
-    #create table query
+    # create table query
     cursor.query(create_table_query).df()
 
     files = os.listdir(path)
-    for file in files:
+    for i in range(len(files)):
+        files[i] = f"{path}/{files[i]}"
+
+    add_documents(files)
+
+    # drop pdfs table now
+    cursor.query(drop_pdfs_query).df()
+
+    print("MyDocuments table initialized!")
+
+
+# Create database MyDocuments from files stored in pdfs/
+def add_documents(list_of_documents: list[str]) -> None:
+    load_pdfs_query = "LOAD PDF '{}' INTO MyPDFs"
+    count_pdfs = 0
+    for file in list_of_documents:
         if file.endswith(".pdf"):
             # add each pdf file while also checking extension
-            cursor.query(load_pdfs_query.format(path, file)).df()
+            cursor.query(load_pdfs_query.format(file)).df()
+            count_pdfs += 1
         elif file.endswith(".txt"):
             # add each text file by calling insert_text_file
-            insert_text_file(file, f"{path}/{file}")
-    
-    merge_pdfs()
+            insert_text_file(f"{file}")
 
-    print("MyDocuments table initialized")
+    if count_pdfs > 0:
+        merge_pdfs()
 
 
 # Create sentence feature extractor function
@@ -152,7 +168,7 @@ def get_query_results(search_query: str, limit: int = PARAGRAPHS_LIMIT) -> Order
     documents_dictionary = OrderedDict()
 
     for index, row in scored_documents_df.iterrows():
-        documents_dictionary[index]=[]
+        documents_dictionary[index] = []
 
     for index, row in scored_paragraphs_df.iterrows():
         document_name = row["scored_paragraphs.name"]
@@ -160,6 +176,8 @@ def get_query_results(search_query: str, limit: int = PARAGRAPHS_LIMIT) -> Order
         paragraph_data = row["scored_paragraphs.data"]
         similarity_score = row["scored_paragraphs.distance"]
         documents_dictionary[document_name].append((paragraph_id, paragraph_data, similarity_score))
+
+    # print(documents_dictionary)
 
     return documents_dictionary
 
@@ -173,13 +191,13 @@ def return_results(documents_dictionary: OrderedDict) -> str:
         for i in value:
             output.append("Relevant Text {}".format(count))
             output.append(i[1])
-            count+=1
+            count += 1
         output.append("-----------------------------------------------")
 
     return '\n'.join(output)
 
 
-# Summarize with LLM if needed
+# Summarize with LLM (Facebook bart cnn) if needed
 def summarize_with_LLM(documents_dictionary: OrderedDict) -> str:
     # First develop prompt
     prompt = []
@@ -234,30 +252,34 @@ def create_text_summarizer():
 
 # initialize all tables and functions
 def initialize():
-    create_my_documents()
+    read_and_store_documents()
     create_sentence_feature_extractor()
     create_text_summarizer()
     create_embeddings_vector_index()
 
 
 def process_one_query() -> bool:
+    global user_defined_summarize
+    global user_defined_limit
     query = input("Query: ")
     if query == "exit":
         print("Exiting...")
         return True
     elif query == "SUMMARIZE":
         print("Summarization enabled!")
-        summarize = True
+        user_defined_summarize = True
     elif query == "NOT SUMMARIZE":
         print("Summarization disabled!")
-        summarize = False
+        user_defined_summarize = False
     elif query.startswith("LIMIT"):
         l = query.split()
         if len(l) == 2 and l[1].isdecimal():
-            limit = int(l[1])
-            print("Limit set to {}".format(limit))
+            user_defined_limit = int(l[1])
+            print("Limit set to {}".format(user_defined_limit))
         else:
             print("Invalid input for LIMIT. Syntax: LIMIT INTEGER")
+    elif query == "SHOW":
+        print(cursor.query("SELECT * FROM MyDocuments").df())
     else:
         documents_dictionary = get_query_results(query, user_defined_limit)
         if user_defined_summarize:
@@ -269,25 +291,49 @@ def process_one_query() -> bool:
 
 # This function repeatedly checks docs folder every 10 seconds and updates database
 # if docs folder has changed.
-def poll_and_update_table():
-    with polling_lock:
-        print("\n\nPolling and updating database if needed\n\n")
+def poll_and_update_table(path: str = DEFAULT_DOCS_PATH):
+    drop_pdfs_query = "DROP TABLE IF EXISTS MyPDFs"
+    delete_doc_query = "DELETE FROM MyDocuments WHERE name = '{}'"
 
-    threading.Timer(10, poll_and_update_table).start()
+    # drop PDFs table if needed
+    cursor.query(drop_pdfs_query).df()
+
+    select_docs_query = "SELECT name FROM MyDocuments"
+
+    docs_df = cursor.query(select_docs_query).df()
+    stored_docs = set(docs_df['mydocuments.name'].to_list())
+
+    docs_in_directory = set()
+    directory_list = os.listdir(path)
+
+    for doc in directory_list:
+        docs_in_directory.add(f"{path}/{doc}")
+
+    docs_to_remove = stored_docs.difference(docs_in_directory)
+    docs_to_add = docs_in_directory.difference(stored_docs)
+
+    for doc in docs_to_remove:
+        cursor.query(delete_doc_query.format(doc))
+
+    add_documents(list(docs_to_add))
+
+    # drop pdfs
+    cursor.query(drop_pdfs_query).df()
+
+    print("\n"*2 + f"Updated the database!\nAdded {', '.join(list(docs_to_add))}\nRemoved {', '.join(list(docs_to_remove))}" + "\n"*2)
 
 
 # Complete end to end flow of query from user
 def process_query():
     initialize()
-    poll_and_update_table()
     print("Initialized all tables and functions")
+    curr_time = time.time()
     while True:
-        should_break = False
-        with polling_lock:
-            should_break = process_one_query()
-
-        if should_break:
-            break
+        if time.time() - curr_time >= polling_interval:
+            poll_and_update_table()
+            curr_time = time.time()
+        should_break = process_one_query()
+        if should_break: break
 
 
 # Main program running process_query to get queries from users.
